28a778771f666bddc0519bc23748f62175148958
Sean Olson <srolson@callutheran.edu>
2019-08-14 10:59:58 -0500

    getting this.state.team_name to show on DOM when user selects team on register page

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -8,6 +8,7 @@ import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 
 const styles = theme => ({
@@ -92,6 +93,111 @@ class RegisterPage extends Component {
   console.log(this.state)
   const { classes } = this.props;
   const { open } = this.state;
+  if (this.state.team === '') {
+      return (
+        <div>
+          {this.props.errors.registrationMessage && (
+            <h2
+              className="alert"
+              role="alert"
+            >
+              {this.props.errors.registrationMessage}
+            </h2>
+          )}
+          <form onSubmit={this.registerUser}>
+            <h1>Register User</h1>
+            <div>
+              <label htmlFor="username">
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleInputChangeFor('username')}
+                />
+              </label>
+            </div>
+            <label htmlFor="username">
+            <div className={classes.root}>
+          {/* <Paper className={classes.paper}>
+            <MenuList>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>My account</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Paper> */}
+          <div>
+            <Button
+              buttonRef={node => {
+                this.anchorEl = node;
+              }}
+              aria-owns={open ? 'menu-list-grow' : undefined}
+              aria-haspopup="true"
+              onClick={this.handleToggle}
+            >
+              SELECT TEAM
+            </Button>
+            <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  id="menu-list-grow"
+                  style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={this.handleClose}>
+                      <MenuList>
+                        {this.props.state.teamsReducer.map(item => {
+                     return(
+                      <div id={item.id}>
+                        <MenuItem onClick={event => this.handleClose(event, item)}>{item.team_name}</MenuItem>                   
+                      </div>
+           )
+         })}   
+                      <div>
+                        <p>{this.state.team_name}</p>
+                      </div>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </div>
+        </div>
+            </label>
+            <div>
+              <label htmlFor="password">
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleInputChangeFor('password')}
+                />
+              </label>
+            </div>
+            <div>
+              <input
+                className="register"
+                type="submit"
+                name="submit"
+                value="Register"
+              />
+            </div>
+          </form>
+          <center>
+            <button
+              type="button"
+              className="link-button"
+              onClick={() => {this.props.dispatch({type: 'SET_TO_LOGIN_MODE'})}}
+            >
+              Login
+            </button>
+          </center>
+        </div>
+      );
+  } else if (this.state.team === 'Minneapolis Mayhem') {
     return (
       <div>
         {this.props.errors.registrationMessage && (
@@ -150,9 +256,8 @@ class RegisterPage extends Component {
                     <div id={item.id}>
                       <MenuItem onClick={event => this.handleClose(event, item)}>{item.team_name}</MenuItem>                   
                     </div>
-         )
-       })} 
-                      
+                  )
+            })}   
                     </MenuList>
                   </ClickAwayListener>
                 </Paper>
@@ -160,16 +265,13 @@ class RegisterPage extends Component {
             )}
           </Popper>
         </div>
-      </div>
-          
-          
-          
-
-
-
-
+          </div>
           </label>
-         
+          <div>
+          <Typography>
+            {this.state.team_name}
+          </Typography>
+          </div>
           <div>
             <label htmlFor="password">
               Password:
@@ -201,8 +303,110 @@ class RegisterPage extends Component {
         </center>
       </div>
     );
+  } else if (this.state.team_name === 'Madison Minotaurs') {
+    return (
+      <div>
+      {this.props.errors.registrationMessage && (
+        <h2
+          className="alert"
+          role="alert"
+        >
+          {this.props.errors.registrationMessage}
+        </h2>
+      )}
+      <form onSubmit={this.registerUser}>
+        <h1>Register User</h1>
+        <div>
+          <label htmlFor="username">
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChangeFor('username')}
+            />
+          </label>
+        </div>
+        <label htmlFor="username">
+        <div className={classes.root}>
+      {/* <Paper className={classes.paper}>
+        <MenuList>
+          <MenuItem>Profile</MenuItem>
+          <MenuItem>My account</MenuItem>
+          <MenuItem>Logout</MenuItem>
+        </MenuList>
+      </Paper> */}
+      <div>
+        <Button
+          buttonRef={node => {
+            this.anchorEl = node;
+          }}
+          aria-owns={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleToggle}
+        >
+          SELECT TEAM
+        </Button>
+        <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              id="menu-list-grow"
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={this.handleClose}>
+                  <MenuList>
+                    {this.props.state.teamsReducer.map(item => {
+                 return(
+                  <div id={item.id}>
+                    <MenuItem onClick={event => this.handleClose(event, item)}>{item.team_name}</MenuItem>                   
+                  </div>
+       )
+     })}   
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </div>
+        </label>
+        <div>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChangeFor('password')}
+            />
+          </label>
+        </div>
+        <div>
+          <input
+            className="register"
+            type="submit"
+            name="submit"
+            value="Register"
+          />
+        </div>
+      </form>
+      <center>
+        <button
+          type="button"
+          className="link-button"
+          onClick={() => {this.props.dispatch({type: 'SET_TO_LOGIN_MODE'})}}
+        >
+          Login
+        </button>
+      </center>
+    </div>
+  );
   }
 }
+} 
 
 // Instead of taking everything from state, we just want the error messages.
 // if you wanted you could write this code like this:
